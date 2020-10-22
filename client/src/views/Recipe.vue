c6bf261be0a646b9382ec1f3c063e47bf21006c9
adneja <aadne.jacobsen@gmail.com>
2020-10-23 00:29:00 +0200

    smooth scroll to top

--- client/src/views/Recipe.vue
+++ client/src/views/Recipe.vue
@@ -275,7 +275,11 @@
             },
 
             scrollToTop() {
-                window.scrollTo(0,0);
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
             },
 
             addIngredientsToShoppingList() {
